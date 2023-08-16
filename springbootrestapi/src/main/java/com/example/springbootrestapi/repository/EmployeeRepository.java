df5cc389230c16f39ec53bce0b212449aa082b36
Nilay <54245048+dnilay@users.noreply.github.com>
2023-08-16 10:07:49 +0530

    Add files via upload

---
+++ springbootrestapi/src/main/java/com/example/springbootrestapi/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.example.springbootrestapi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.springbootrestapi.entity.Employee;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
